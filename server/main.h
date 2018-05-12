37173c78a4d76dd28e675a13b1c897733b6198cb
mily20001 <mily20001@gmail.com>
2018-05-12 19:49:20 +0200

    limitowanie maksymalnej dlugosci pakietu

---
+++ server/main.h
@@ -0,0 +1,30 @@
+#ifndef SERVER_MAIN_H
+#define SERVER_MAIN_H
+
+#include <cerrno>
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/shm.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
+#include <google/protobuf/message.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+
+#include "protbuf/messages.pb.h"
+
+
+#define MAX_PACKET_SIZE 4096
+
+#endif //SERVER_MAIN_H
