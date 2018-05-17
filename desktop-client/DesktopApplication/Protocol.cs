e254c94b9b64551a6599bfde78c5711664e52189
culring <ravw96@gmail.com>
2018-05-17 11:52:50 +0200

    Decode/Encode messages implemented. Socket connections outline.

--- desktop-client/DesktopApplication/Protocol.cs
+++ desktop-client/DesktopApplication/Protocol.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Google.Protobuf;
 using StorageCloud.Desktop.Protobuf;
+using HashAlgorithm = StorageCloud.Desktop.Protobuf.HashAlgorithm;
 
 namespace StorageCloud.Desktop
 {
@@ -12,38 +15,49 @@ namespace StorageCloud.Desktop
     {
         struct Packet
         {
-            public MessageType type;
-            public IMessage message;
+            public MessageType Type;
+            public IMessage Message;
         }
 
-        // parse received packet
-        static void DecodeMessage(byte[] data)
+        // prepare a packet to be sent
+        static byte[] EncodeMessage(Packet packet)
         {
-            EncodedMessage message = EncodedMessage.Parser.ParseFrom(data);
-            Packet packet = GetPacketFromEncodedMessage(message);
+            EncodedMessage encodedMessage = CreateEncodedMessageFromPacket(packet);
+            byte[] serializedMessage = encodedMessage.ToByteArray();
+            return serializedMessage;
         }
 
-        // prepare packet to be sent
-        static void EncodeMessage(Packet packet)
+        // parse a received packet
+        static Packet DecodeMessage(byte[] data)
         {
-
+            // parse the encoded message from the bytes
+            EncodedMessage encodedMessage = EncodedMessage.Parser.ParseFrom(data);
+            // chech hashes
+            byte[] incomingHash = encodedMessage.Hash.ToByteArray();
+            byte[] currentHash = CalculateHash(encodedMessage.Data.ToByteArray(), HashAlgorithm.HSha512);
+            if (CompareHash(incomingHash, currentHash))
+            {
+                throw new InvalidOperationException("The hash in the incoming packet does not match with the computed hash");
+            }
+            // return the packet from the encoded message
+            return CreatePacketFromEncodedMessage(encodedMessage);
         }
 
-        private static Packet GetPacketFromEncodedMessage(EncodedMessage encodedMessage)
+        private static Packet CreatePacketFromEncodedMessage(EncodedMessage encodedMessage)
         {
             IMessage innerMesage = 
                 GetMessageFromEncodedMessage(encodedMessage.Type, encodedMessage.Data.ToByteArray());
             Packet packet = new Packet()
             {
-                type = encodedMessage.Type,
-                message = innerMesage
+                Type = encodedMessage.Type,
+                Message = innerMesage
             };
             return packet;
         }
 
         private static IMessage GetMessageFromEncodedMessage(MessageType type, byte[] data)
         {
-            DecipherData(data);
+            DecryptData(data, EncryptionAlgorithm.Caesar);
             switch (type)
             {
                 case MessageType.ServerResponse:
@@ -59,21 +73,93 @@ namespace StorageCloud.Desktop
             }
         }
 
-        private static void CheckHash()
+        // true if hashes are equal
+        private static bool CompareHash(byte[] hash1, byte[] hash2)
         {
-
+            return Enumerable.SequenceEqual(hash1, hash2);
         }
 
-        private static void DecipherData(byte[] data)
+        private static void EncryptData(byte[] data, EncryptionAlgorithm encryptionAlgorithm)
         {
-            for (var i = 0; i < data.Length; ++i)
+            switch (encryptionAlgorithm)
             {
-                if (data[i] == 0)
-                {
-                    data[i] = 255;
-                }
-                else data[i] -= 1;
+                case EncryptionAlgorithm.Null4:
+                    throw new NotImplementedException("Null4 encryption algorithm has not been implemented yet");
+                case EncryptionAlgorithm.Noencryption:
+                    break;
+                case EncryptionAlgorithm.Caesar:
+                    for (int i = 0; i < data.Length; ++i)
+                    {
+                        data[i] = data[i] == 255 ? (byte)0 : (byte)(data[i] + 1);
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(encryptionAlgorithm), encryptionAlgorithm, null);
             }
+        }
+
+        private static void DecryptData(byte[] data, EncryptionAlgorithm encryptionAlgorithm)
+        {
+            switch (encryptionAlgorithm)
+            {
+                case EncryptionAlgorithm.Null4:
+                    throw new NotImplementedException("Null4 encryption algorithm has not been implemented yet");
+                case EncryptionAlgorithm.Noencryption:
+                    break;
+                case EncryptionAlgorithm.Caesar:
+                    for (var i = 0; i < data.Length; ++i)
+                    {
+                        data[i] = data[i] == 0 ? (byte)255 : (byte)(data[i] - 1);
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(encryptionAlgorithm), encryptionAlgorithm, null);
+            }
+        }
+
+        private static EncodedMessage CreateEncodedMessageFromPacket(Packet packet)
+        {
+            // serialize and encrypt packet message
+            byte[] data = packet.Message.ToByteArray();
+            EncryptData(data, EncryptionAlgorithm.Caesar);
+            // create Encoded Message
+            EncodedMessage encodedMessage = new EncodedMessage()
+            {
+                DataSize = (ulong)packet.Message.CalculateSize(),
+                HashAlgorithm = HashAlgorithm.HSha512,
+                Hash = ByteString.CopyFrom(CalculateHash(data, HashAlgorithm.HSha512)),
+                Type = packet.Type,
+                Data = ByteString.CopyFrom(data)
+            };
+
+            return encodedMessage;
+        }
+
+        private static byte[] CalculateHash(byte[] data, HashAlgorithm algorithm)
+        {
+            switch (algorithm)
+            {
+                case HashAlgorithm.Null2:
+                    throw CreateNotImplementedException("Null2");
+                case HashAlgorithm.HNohash:
+                    byte[] bytes = {new byte()};
+                    return bytes;
+                case HashAlgorithm.HSha256:
+                    return SHA256.Create().ComputeHash(data);
+                case HashAlgorithm.HSha512:
+                    return SHA512.Create().ComputeHash(data);
+                case HashAlgorithm.HSha1:
+                    return SHA1.Create().ComputeHash(data);
+                case HashAlgorithm.HMd5:
+                    return MD5.Create().ComputeHash(data);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(algorithm), algorithm, null);
+            }
+        }
+
+        private static NotImplementedException CreateNotImplementedException(string prefix)
+        {
+            return new NotImplementedException(prefix + " has not been implemented yet");
         }
 
         static void Main(string[] args)
---
+++ desktop-client/DesktopApplication/Server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StorageCloud.Desktop
+{
+    class Server
+    {
+        public void createListener()
+        {
+            // Create an instance of the TcpListener class.
+            TcpListener tcpListener = null;
+            IPAddress ipAddress = Dns.GetHostEntry("localhost").AddressList[0];
+            try
+            {
+                // Set the listener on the local IP address
+                // and specify the port.
+                tcpListener = new TcpListener(ipAddress, 13);
+                tcpListener.Start();
+                Console.WriteLine("Waiting for a connection...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            while (true)
+            {
+                // Always use a Sleep call in a while(true) loop
+                // to avoid locking up your CPU.
+                Thread.Sleep(10);
+                // Create a TCP socket.
+                // If you ran this server on the desktop, you could use
+                // Socket socket = tcpListener.AcceptSocket()
+                // for greater flexibility.
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                // Read the data stream from the client.
+                byte[] bytes = new byte[256];
+                NetworkStream stream = tcpClient.GetStream();
+                stream.Read(bytes, 0, bytes.Length);
+                SocketHelper helper = new SocketHelper();
+                helper.processMsg(tcpClient, stream, bytes);
+            }
+        }
+    }
+}
